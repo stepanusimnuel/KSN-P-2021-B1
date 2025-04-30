857cf940e8dac7d9f6a327ef7eab55beaf84763e
stepanusimnuel <nuel597@gmail.com>
2025-04-30 17:22:29 +0700

    template c++ file

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,n) for(int i=1;i<=n;i++)
+#define mod1 1000000007
+#define mod2 998244353
+#define pb push_back
+#define vi vector<int>
+#define ii pair<int, int>
+#define ff first
+#define ss second
+#define lli long long int
+#define INF 1000000000
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+	// ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	int t, n;
+	cin >> t;
+
+	while(t--) {
+		cin >> n;
+
+		
+	}
+
+	return 0;
+}
+
+
+
